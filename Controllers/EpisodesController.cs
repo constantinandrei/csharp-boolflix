6e143d5c7495e26caddb8cd2b31c1ed972594181
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-12-07 14:39:59 +0100

    aggiunto controller per episodi

---
+++ Controllers/EpisodesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using csharp_boolflix.Areas.Identity.Data;
+using csharp_boolflix.Models;
+
+namespace csharp_boolflix.Controllers
+{
+    public class EpisodesController : Controller
+    {
+        private readonly IdentityDataContext _context;
+
+        public EpisodesController(IdentityDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Episodes
+        public async Task<IActionResult> Index()
+        {
+            var identityDataContext = _context.Episodes.Include(e => e.Season);
+            return View(await identityDataContext.ToListAsync());
+        }
+
+        // GET: Episodes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Episodes == null)
+            {
+                return NotFound();
+            }
+
+            var episode = await _context.Episodes
+                .Include(e => e.Season)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (episode == null)
+            {
+                return NotFound();
+            }
+
+            return View(episode);
+        }
+
+        // GET: Episodes/Create
+        public IActionResult Create()
+        {
+            ViewData["SeasonId"] = new SelectList(_context.Seasons, "Id", "Id");
+            return View();
+        }
+
+        // POST: Episodes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,SeasonId,OrderInSeason")] Episode episode)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(episode);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SeasonId"] = new SelectList(_context.Seasons, "Id", "Id", episode.SeasonId);
+            return View(episode);
+        }
+
+        // GET: Episodes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Episodes == null)
+            {
+                return NotFound();
+            }
+
+            var episode = await _context.Episodes.FindAsync(id);
+            if (episode == null)
+            {
+                return NotFound();
+            }
+            ViewData["SeasonId"] = new SelectList(_context.Seasons, "Id", "Id", episode.SeasonId);
+            return View(episode);
+        }
+
+        // POST: Episodes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,SeasonId,OrderInSeason")] Episode episode)
+        {
+            if (id != episode.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(episode);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EpisodeExists(episode.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SeasonId"] = new SelectList(_context.Seasons, "Id", "Id", episode.SeasonId);
+            return View(episode);
+        }
+
+        // GET: Episodes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Episodes == null)
+            {
+                return NotFound();
+            }
+
+            var episode = await _context.Episodes
+                .Include(e => e.Season)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (episode == null)
+            {
+                return NotFound();
+            }
+
+            return View(episode);
+        }
+
+        // POST: Episodes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Episodes == null)
+            {
+                return Problem("Entity set 'IdentityDataContext.Episodes'  is null.");
+            }
+            var episode = await _context.Episodes.FindAsync(id);
+            if (episode != null)
+            {
+                _context.Episodes.Remove(episode);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EpisodeExists(int id)
+        {
+          return _context.Episodes.Any(e => e.Id == id);
+        }
+    }
+}
