a3c359bc0fac866b2be96e560d19fab7cdcdedbd
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-12-07 13:52:21 +0100

    commit prima di cambiare le classi nel db context

---
+++ Controllers/SeasonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_boolflix.Controllers
+{
+    [Authorize]
+    public class SeasonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
